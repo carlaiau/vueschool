3afb425ace98ef715908198be8526547a86d5152
Carl Aiau <carl@ortus.co.nz>
2019-07-22 09:20:16 +1200

    404 page

---
+++ forum/src/pages/PageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-full">
+    <h1>Sorry this does not exist</h1>
+    <p>
+        Why don't you go 
+        <router-link :to="{name:'Home'}">
+            home
+        </router-link> 
+        instead?
+    </p>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+
+</style>
