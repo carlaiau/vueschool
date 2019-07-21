e1daf4dfa7c0ef022e21af6c7bd8b0fb1c8fec35
Carl Aiau <carl@ortus.co.nz>
2019-07-22 11:24:27 +1200

    Submission of Posts (Mutation of global state at root element, via custom event emitter)

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,48 @@
+<template>
+    <form @submit.prevent="save"> 
+            <div class="form-group">
+                <textarea 
+                    class="form-input"
+                    
+                    v-model="text"
+                ></textarea>
+            </div>
+            <div class="form-actions">
+                <button class="btn-blue">Submit Post</button>
+            </div>
+    </form>
+</template>
+
+<script>
+import sourceData from '@/data'
+export default {
+    data () {
+        return {
+            thread: sourceData.threads[this.id],
+            text: ''
+        }
+    },
+    props: {
+        threadId: {
+            required: true
+        }
+    },
+    methods: {
+        save () {
+            const postId = 'greatPost' + Math.random()
+            const post = {
+                text: this.text,
+                publishedAt: Math.floor(Date.now() / 100),
+                threadId: this.threadId,
+                userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2', // Random selection of Rolf,
+                '.key': postId
+            }
+            // Reset form input
+            this.text = ''
+
+            // This is returning the new post back to parent
+            this.$emit('save', {post})
+        }
+    }
+}
+</script>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,15 +2,21 @@
     <div class="col-large push-top">
         <h1>{{thread.title}}</h1>
         <PostList :posts="posts"/>
+        <PostEditor
+            @save="addPost"
+            :threadId="id"
+        />
     </div>
 </template>
 
 <script>
     import sourceData from '@/data'
     import PostList from '@/components/PostList'
+    import PostEditor from '@/components/PostEditor'
     export default {
         components: {
-            PostList
+            PostList,
+            PostEditor
         },
         props: {
             id: {
@@ -28,6 +34,18 @@
                 const postIds = Object.values(this.thread.posts)
                 return Object.values(sourceData.posts)
                     .filter(post => postIds.includes(post['.key']))
+            }
+        },
+        methods: {
+            addPost (eventData) {
+                console.log(eventData)
+                const post = eventData.post
+                const postId = eventData.post['.key']
+
+                // Actually amend the global state
+                this.$set(sourceData.posts, postId, post)
+                this.$set(this.thread.posts, postId, postId)
+                this.$set(sourceData.users[post.userId].posts, postId, postId)
             }
         }
     }
